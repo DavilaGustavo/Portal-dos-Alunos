89266b5bca3fecfe3fa484a9667d462f0c895b6c
Duarte-G <bieldusantos07@gmail.com>
2023-11-25 14:51:08 -0300

    Implementacao da biblioteca dos alunos

---
+++ documentacao/src/Alunos.h
@@ -0,0 +1,32 @@
+#ifndef ALUNOS_H
+#define ALUNOS_H
+
+#include <string>
+
+using namespace std;
+
+class Alunos {
+private:
+    string nome;
+    string email;
+    int RA;
+    int periodo;
+
+public:
+    // Construtor
+    Alunos(const string& nome, const string& email, int RA, int periodo);
+
+    // Métodos de Acesso
+    void setNome(const string& nome);
+    void setEmail(const string& email);
+    void setRegistro(int RA);
+    void setPeriodo(int periodo);
+
+    string getNome () const;
+    string getEmail() const;
+    int getRegistro() const;
+    int getPeriodo() const;
+};
+
+
+#endif

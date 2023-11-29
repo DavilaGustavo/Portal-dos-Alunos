8d43e0ba6c5a49eb3557184e103777e69e32c3cc
DavilaGustavo <gustavogdavila@gmail.com>
2023-11-28 21:56:37 -0300

    Codigo organizado e comentado

--- documentacao/src/Professores.cpp
+++ documentacao/src/Professores.cpp
@@ -2,32 +2,26 @@
 
 // Construtor
 Professores::Professores(const string& nome, const string& email)
-        : nome(nome), email(email){
+        : nome(nome), email(email){}
 
-        }
-
-// Metodos de Acesso
-
+// Funções para trocar as variaveis privadas
 void Professores::setNome(const string& nome){
     this->nome = nome;
 }
-
 void Professores::setEmail(const string& email){
     this->email = email;
 }
-
 void Professores::adicionarMaterias(const string& materia){
     materias.push_back(materia);
 }
 
+// Funções para ler e devolver os valores das variaveis privadas
 string Professores::getNome() const{
     return nome;
 }
-
 string Professores::getEmail() const{
     return email;
 }
-
 vector<string> Professores::getMaterias() const{
     return materias;
 }
--- documentacao/src/main.cpp
+++ documentacao/src/main.cpp
@@ -5,9 +5,11 @@
 using namespace std;
 
 int main() {
+    // Inicializando as classes aluno/professor com informações a fim de teste
     Alunos aluno("Gabriel Duarte", "gabrielduarte@alunos.utfpr.edu.br", 2382945, 5);
     Professores professor("Ricardo Tavares", "ricardo103@professores.utfpr.edu.br");
 
+    // Chamado para testar as funções get()
     cout << "Nome: " << aluno.getNome() << endl;
     cout << "Email: " << aluno.getEmail() << endl;
     cout << "RA: " << aluno.getRA()  << endl;
@@ -15,11 +17,15 @@ int main() {
     
     cout << endl;
     
+    // Chamado para testar a função adicionar()
     professor.adicionarMaterias("Estrutura de Dados");
     professor.adicionarMaterias("Teoria da Computacao");
+
     cout << "Nome: " << professor.getNome() << endl;
     cout << "Email: " << professor.getEmail() << endl;
     cout << "Materias dadas: ";
+
+    // Loop para printar todas as materias que tal professor possui
     for (const auto& materia : professor.getMaterias()){
         cout << materia << " " << endl;
     }
